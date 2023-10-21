676fd9794ad011443c4c25ad022fcf60f6422e02
kivavik <dpkivva@mail.ru>
2023-10-21 06:08:13 +0300

    added If_Else

---
+++ If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter user name!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow? Masha!");
+}
+else
+{
+    Console.WriteLine("Beee!");
+}
